805d37011926b0b4eaf2f08daa3c475ec27babd6
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-05-27 09:26:00 +0200

    release v0.2.1

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,8 +3,10 @@ local lib = require "rr-lib"
 -- Recursively add science-packs if prerequisites have it
 if settings.startup["rr-py-integrity"].value then 
   local depth = settings.startup["rr-py-depth"].value
-  
-  for tech, ___ in pairs(data.raw.technology) do
-    lib.add_prerequisite_packs(tech, depth --[[@as number]])
+  while depth > 0 do
+    for tech, ___ in pairs(data.raw.technology) do
+      lib.add_prerequisite_packs(tech, 1)
+    end
+    depth = depth - 1
   end
 end
